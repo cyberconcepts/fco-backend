e53d8adc60bcfa545ba14504ed3fa7ce0db6d07c
Helmut Merz <helmutm@cy55.de>
2019-02-08 15:38:12 +0100

    remove obsolete legacy implementation

--- src/Fco/Backend/Demo.hs
+++ src/Fco/Backend/Demo.hs
@@ -8,107 +8,49 @@ module Fco.Backend.Demo (run) where
 
 import BasicPrelude
 
-import Control.Distributed.Process (
-    Process, ProcessId, ReceivePort, SendPort,
-    getSelfPid, matchChan, newChan, 
-    receiveChanTimeout, receiveWait, sendChan)
 import Control.Monad.Extra (whileM)
 
 import Fco.Backend (setupEnv)
 import Fco.Backend.Service (
     BackendRequest (..), BackendRespChannel, BackendResponse (..),
     BackendService, 
-    startBackendSvc,
-    Request (..), Response (..), ResponseChan,
-    setupBackend)
+    startBackendSvc)
 import Fco.Backend.Types (dbSettings, dbName, envDB, environment)
-import Fco.Core.Config (
-    startConfigSvcDefault,
-    setupConfigDef)
-import Fco.Core.Console (setupConsole)
-import Fco.Core.Messaging (
-    CtlChan, CtlMsg (DoQuit, InfoMsg), 
-    NotifChan, Notification (RequestQuit),
-    runMainProcess)
+import Fco.Core.Config (startConfigSvcDefault)
 import qualified Fco.Core.Parse as CP
+import Fco.Core.Service (
+    Channel, HandledChannel (..), Message (..), Service (..),
+    conIn, conOutHandler, defaultCtlHandler, defaultListener, dummyHandler, 
+    newChan, receiveChanAny, send, startService)
 import qualified Fco.Core.Show as CS
 import Fco.Core.Types (Namespace (..))
 
-import qualified Fco.Core.Service as Svc
-import Fco.Core.Service (Channel, HandledChannel (..),
-    conOutHandler,
-    defaultCtlHandler, defaultListener, dummyHandler, 
-    receiveChanAny,
-    startService)
 
-
--- new implementation, using Fco.Core.ServiceId
-
-inpHandler :: BackendService -> BackendRespChannel -> Svc.Message Text -> IO Bool
-inpHandler backend respChan (Svc.Message txt) = do
-    Svc.send backend $
-            Svc.Message (BackendQuery respChan (CP.parseQuery (Namespace "") txt))
+inpHandler :: BackendService -> BackendRespChannel -> Message Text -> IO Bool
+inpHandler backend respChan (Message txt) = do
+    send backend $
+            Message (BackendQuery respChan (CP.parseQuery (Namespace "") txt))
     return True
-inpHandler backend respChan (Svc.QuitMsg) = 
-    (Svc.send backend Svc.QuitMsg) >> return False
+inpHandler backend respChan (QuitMsg) = 
+    (send backend QuitMsg) >> return False
 
-responseHandler :: Svc.Service Text -> Svc.Message BackendResponse -> IO Bool
-responseHandler conout (Svc.Message (BackendResponse triples)) = do
-    Svc.send conout $ Svc.Message (unlines (map CS.showTriple triples))
+responseHandler :: Service Text -> Message BackendResponse -> IO Bool
+responseHandler conout (Message (BackendResponse triples)) = do
+    send conout $ Message (unlines (map CS.showTriple triples))
     return True
+
 
 run :: IO ()
 run = do
-    conRecvChan <- Svc.newChan
-    backendRespChan <- Svc.newChan
+    conRecvChan <- newChan
+    backendRespChan <- newChan
     configSvc <- startConfigSvcDefault
     let db = dbSettings { dbName = "fco_test" }
     env <- setupEnv $ environment { envDB = db }
     backendSvc <- startBackendSvc env
-    conInSvc <- startService (Svc.conIn conRecvChan) dummyHandler ()
+    conInSvc <- startService (conIn conRecvChan) dummyHandler ()
     conOutSvc <- startService defaultListener conOutHandler ()
     whileM $ receiveChanAny [
         HandledChannel conRecvChan (inpHandler backendSvc backendRespChan),
         HandledChannel backendRespChan (responseHandler conOutSvc)]
-    return ()
 
-
--- legacy stuff, using distributed-process
-
-handleNotif :: Notification -> Process Bool
-handleNotif (RequestQuit service) = return False
-handleNotif _ = return True
-
-handleConMsg :: SendPort Request -> SendPort Response -> Text -> Process Bool
-handleConMsg reqSend respSend txt = 
-    sendChan reqSend (Query respSend (CP.parseQuery (Namespace "") txt))
-    >> return True
-
-handleBeResp :: SendPort Text -> Response -> Process Bool
-handleBeResp port (Response svcId triples) = 
-    sendChan port (unlines (map CS.showTriple triples))
-    >> return True
-
-
-run_x :: IO ()
-run_x = 
-  runMainProcess $ do
-    (self, notifRecv) <- newChan :: Process NotifChan
-    (cfgReqSend, cfgId) <- setupConfigDef self
-    (conWSend, conRRecv, conWId, conRId) <- setupConsole self --cfgReqSend
-    let db = dbSettings { dbName = "fco_test" }
-    env <- liftIO $ setupEnv $ environment { envDB = db }
-    (beReqSend, beId) <- setupBackend self env
-    (beRespSend, beRespRecv) <- newChan :: Process ResponseChan
-    whileM $
-      receiveWait [
-          matchChan notifRecv $ handleNotif,
-          matchChan conRRecv $ handleConMsg beReqSend beRespSend,
-          matchChan beRespRecv $ handleBeResp conWSend
-      ]
-    sendChan cfgId DoQuit
-    sendChan conWId DoQuit
-    sendChan beId DoQuit
-    receiveChanTimeout 1000000 notifRecv >>= print
-    receiveChanTimeout 1000000 notifRecv >>= print
-    receiveChanTimeout 1000000 notifRecv >>= print
--- src/Fco/Backend/Service.hs
+++ src/Fco/Backend/Service.hs
@@ -8,39 +8,25 @@
 module Fco.Backend.Service (
     BackendRequest (..), BackendRespChannel, BackendResponse (..), 
     BackendService, 
-    startBackendSvc,
-    Request (..), Response (..), ResponseChan,
-    setupBackend) where
+    startBackendSvc) where
 
 import BasicPrelude
 import qualified Data.Text as T
 
-import Data.Binary (Binary)
 import Data.IntMap (elems)
-import GHC.Generics (Generic)
-
-import Control.Distributed.Process (
-    Process, ReceivePort, SendPort,
-    newChan, sendChan, spawnLocal)
 
 import Fco.Backend (query, storeTriple)
 import Fco.Backend.Types (Environment)
-import Fco.Core.Messaging (
-    Channel, CtlChan, Message, MsgHandler, Notification (RequestQuit),
-    ParentId, Service (..), ServiceId,
-    defaultService, setupService,)
 import qualified Fco.Core.Types as CT
 
-import qualified Fco.Core.Service as Svc
 import Fco.Core.Service (
-    defaultCtlHandler, defaultListener, dummyHandler, startService)
+    Channel (..), Message (..), MsgHandler (..), Service (..),
+    defaultCtlHandler, defaultListener, dummyHandler, sendChan, startService)
 
 
--- new implementation, using Fco.Core.ServiceId
+type BackendService = Service BackendRequest
 
-type BackendService = Svc.Service BackendRequest
-
-type BackendRespChannel = Svc.Channel BackendResponse
+type BackendRespChannel = Channel BackendResponse
 
 data BackendRequest = BackendQuery BackendRespChannel CT.Query
                     | BackendUpdate CT.Triple
@@ -51,47 +37,13 @@ newtype BackendResponse = BackendResponse [CT.Triple]
 startBackendSvc :: Environment -> IO BackendService
 startBackendSvc env = startService defaultListener backendHandler env
 
-backendHandler :: Svc.MsgHandler Environment BackendRequest
-backendHandler env (Svc.Message (BackendQuery rchannel qu)) = do
+backendHandler :: MsgHandler Environment BackendRequest
+backendHandler env (Message (BackendQuery rchannel qu)) = do
     tr <- query env qu
-    Svc.sendChan rchannel $ Svc.Message (BackendResponse tr)
+    sendChan rchannel $ Message (BackendResponse tr)
     return $ Just env
-backendHandler env (Svc.Message (BackendUpdate tr)) = do
+backendHandler env (Message (BackendUpdate tr)) = do
     storeTriple env tr
     return $ Just env
 backendHandler env msg = defaultCtlHandler env msg
 
-
--- legacy stuff, using distributed-process
-
-data Request = Query (SendPort Response) CT.Query
-                | Update CT.Triple
-  deriving (Show, Generic, Typeable)
-instance Binary Request
-instance Message Request
-
-data Response = Response ServiceId [CT.Triple]
-  deriving (Show, Generic, Typeable)
-instance Binary Response
-instance Message Response
-
-type ResponseChan = Channel Response
-
-
-setupBackend :: ParentId -> Environment ->
-                Process (SendPort Request, ServiceId)
-setupBackend parent env = 
-    let svc = defaultService { 
-                messageHandler = reqHandler, 
-                serviceState = env }
-    in setupService svc parent
-
-
-reqHandler :: MsgHandler Environment Request
-reqHandler svcId env (Query client qu) = do
-    tr <- liftIO $ query env qu
-    sendChan client $ Response svcId tr
-    return $ Just env
-reqHandler svcId env (Update triple) = do
-    liftIO $ storeTriple env triple
-    return $ Just env

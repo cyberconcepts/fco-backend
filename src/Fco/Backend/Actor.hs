758f2918362129a36d8d3a2366cb8d102631fe5b
Helmut Merz <helmutm@cy55.de>
2019-02-25 13:34:36 +0100

    just another renaming action: 'Channel' is now 'Mailbox'

--- src/Fco/Backend/Actor.hs
+++ src/Fco/Backend/Actor.hs
@@ -15,25 +15,25 @@ import qualified Data.Text as T
 import Data.IntMap (elems)
 
 import Control.Concurrent.Actor (
-    Channel, Message (..), MsgHandler, 
+    Mailbox, Message (..), MsgHandler, 
     defCtlHandler, send, spawnDefActor)
 import Fco.Backend (query, storeTriple)
 import Fco.Backend.Types (Environment)
 import qualified Fco.Core.Types as CT
 
 
-data Request = Query (Channel Response) CT.Query
+data Request = Query (Mailbox Response) CT.Query
              | Update CT.Triple
 
 newtype Response = Response [CT.Triple]
 
-spawnBackend :: Environment -> IO (Channel Request)
+spawnBackend :: Environment -> IO (Mailbox Request)
 spawnBackend env = spawnDefActor backendHandler env
 
 backendHandler :: MsgHandler Environment Request
-backendHandler env (Message (Query rchannel qu)) = do
+backendHandler env (Message (Query respbox qu)) = do
     tr <- query env qu
-    send rchannel $ Message (Response tr)
+    send respbox $ Message (Response tr)
     return $ Just env
 backendHandler env (Message (Update tr)) = do
     storeTriple env tr
--- src/Fco/Backend/Demo.hs
+++ src/Fco/Backend/Demo.hs
@@ -11,8 +11,8 @@ import BasicPrelude
 import Control.Monad.Extra (whileM)
 
 import Control.Concurrent.Actor (
-    Behaviour (..), Channel, Message (..), MsgHandler,
-    defActor, newChan, send, spawnActor, spawnDefActor)
+    Behaviour (..), Mailbox, Message (..), MsgHandler,
+    defActor, mailbox, send, spawnActor, spawnDefActor)
 import Control.Concurrent.Actor.Config (spawnConfigDef)
 import Control.Concurrent.Actor.Console (conIn, conOutHandler)
 import Fco.Backend (setupEnv)
@@ -23,32 +23,32 @@ import qualified Fco.Core.Show as CS
 import Fco.Core.Types (Namespace (..))
 
 
-inpHandler :: Channel Request -> Channel Response -> MsgHandler st Text
-inpHandler reqChan respChan state (Message txt) = do
-    send reqChan $
-            Message (Query respChan (CP.parseQuery (Namespace "") txt))
+inpHandler :: Mailbox Request -> Mailbox Response -> MsgHandler st Text
+inpHandler reqbox respbox state (Message txt) = do
+    send reqbox $
+            Message (Query respbox (CP.parseQuery (Namespace "") txt))
     return $ Just state
-inpHandler reqChan respChan _ QuitMsg = 
-    (send reqChan QuitMsg) >> return Nothing
+inpHandler reqbox _ _ QuitMsg = 
+    (send reqbox QuitMsg) >> return Nothing
 
-responseHandler :: Channel Text -> MsgHandler st Response
-responseHandler out state (Message (Response triples)) = do
-    send out $ Message (unlines (map CS.showTriple triples))
+responseHandler :: Mailbox Text -> MsgHandler st Response
+responseHandler outbox state (Message (Response triples)) = do
+    send outbox $ Message (unlines (map CS.showTriple triples))
     return $ Just state
 
 
 run :: IO ()
 run = do
-    conRecvChan <- newChan
-    backendRespChan <- newChan
-    configChan <- spawnConfigDef
+    inbox <- mailbox
+    respbox <- mailbox
+    confbox <- spawnConfigDef
     let db = dbSettings { dbName = "fco_test" }
     env <- setupEnv $ environment { envDB = db }
-    backendReqChan <- spawnBackend env --configChan
-    spawnActor (conIn conRecvChan) [] ()
-    conOutChan <- spawnDefActor conOutHandler ()
+    reqbox <- spawnBackend env --confbox
+    spawnActor (conIn inbox) [] ()
+    outbox <- spawnDefActor conOutHandler ()
     defActor [
-          Behaviour conRecvChan (inpHandler backendReqChan backendRespChan),
-          Behaviour backendRespChan (responseHandler conOutChan)]
+          Behaviour inbox (inpHandler reqbox respbox),
+          Behaviour respbox (responseHandler outbox)]
         ()
 
